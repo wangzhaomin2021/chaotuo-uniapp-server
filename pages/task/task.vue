604b320584b5e0e83eadbbbb1da77411b04e34b6
wangzhaomin <wangzhaomin@qesong.com>
2024-11-27 18:00:28 +0800

    init

---
+++ pages/task/task.vue
@@ -0,0 +1,18 @@
+<template>
+	<view>
+		task
+		<button @click="addTask">add</button>
+	</view>
+</template>
+
+<script setup>
+	const taskObj = uniCloud.importObject('task')
+	
+	function addTask() {
+		taskObj.addTask(66)
+	}
+</script>
+
+<style>
+
+</style>
